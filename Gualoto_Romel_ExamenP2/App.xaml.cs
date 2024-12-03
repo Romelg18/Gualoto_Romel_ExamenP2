b5f8c3024c6b4120b8570b182925fd2b45453298
rggua <rggua@ROMELLAPTOP>
2024-12-03 07:23:48 -0500

    Agregar archivos de proyecto.

---
+++ Gualoto_Romel_ExamenP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gualoto_Romel_ExamenP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Gualoto_Romel_ExamenP2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Gualoto_Romel_ExamenP2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
